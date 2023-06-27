e4601ce9804e6ad8a958ef059f244692ed92d8e8
Anna Tasheva <anna.tasheva90@gmail.com>
2023-06-27 10:28:55 +0100

    Implemeneted suggested changes in branch nc-news-2 (Ticket 3)

--- controllers/endpoints.controllers.js
+++ controllers/endpoints.controllers.js
@@ -1,7 +1,13 @@
-const { selectEndpoints } = require("../models/endpoints.models");
+const fs = require("fs/promises");
 
 exports.getEndpoints = (req, res, next) => {
-    selectEndpoints()
+    return fs
+        .readFile(`${__dirname}/../endpoints.json`)
+        .then((fileContents) => {
+            const contentsToString = fileContents.toString();
+            const endpoints = JSON.parse(contentsToString);
+            return endpoints;
+        })
         .then((endpoints) => {
             res.status(200).send({ endpoints });
         })
--- models/endpoints.models.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs/promises");
-
-exports.selectEndpoints = () => {
-    return fs.readFile(`${__dirname}/../endpoints.json`).then((fileContents) => {
-        const contentsToString = fileContents.toString();
-        const endpoints = JSON.parse(contentsToString);
-        return endpoints;
-    });
-};
